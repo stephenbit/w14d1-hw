83aa41ea5ca6e7e8e02dc84e0f02eceb8ca698b6
Stephen Ramsay <rabrandom@gmail.com>
2020-03-02 16:11:22 +0000

    added the constructor

---
+++ src/components/Film.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+function Film() {
+    return (
+        <li>
+            <p>A Film</p>
+        </li>
+    );
+}
+
+export default Film;
---
+++ src/components/FilmList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Film from "./Film.js";
+
+class FilmList extends Component {
+  render() {
+    return (
+      <ul>
+        <Film/>
+      </ul>
+    );
+  }
+}
+
+export default FilmList;
